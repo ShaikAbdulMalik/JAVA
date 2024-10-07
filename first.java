055d2bbb98dd03a6d33c3cf9b3b54b707df10cae
benniimaru <shaikabdulmalik185@gmail.com>
2024-10-07 23:37:48 +0530

    print program

---
+++ first.java
@@ -0,0 +1,13 @@
+public class first{
+    public static void main(String[] args){
+        System.out.println("first program Hello world");
+    }
+}// this is a comment 
+
+/* 
+ * 
+ * This is used for a multiline comment
+ * 
+*/
+
+// \n is used to print the output on the next line. 
