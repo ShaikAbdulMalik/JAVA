5ac786f052fe7d9c72db2662d4c5deae1ba91d26
benniimaru <shaikabdulmalik185@gmail.com>
2024-10-09 03:16:29 +0530

    different variables

---
+++ variable.java
@@ -0,0 +1,22 @@
+public class variable {
+    public static void main(String[] args){
+         //int x; //declarration
+         //x=1223; //assignment
+         // int x = 1223; declaration+assignment
+         //float y = 3.14f;
+         //double z = 3.1234567;
+         //boolean b = true/false;
+         //char symbol = '@';
+         String name = "Abdul Malik";   
+        System.out.println("my name is: "+name);
+    }
+}
+//Boolean - 1 bit size returns "true" or "false" value
+//Int- 4 bytes can store upto -2 billon to 2 billion 
+//double- 8 bytes it can store a fractional number upto 15 digits ex: 3.125463542789645
+//char- 2 bytes it can store a single charracter/letterr/asci value ex: "f"
+//string- varies on the letter assinged. it is a sequence of charracters ex"Hello World".
+
+//see more about primitive or refrence datatype. 
+//how to reate a variable?? = int x;- declaration
+//  x = 123;
