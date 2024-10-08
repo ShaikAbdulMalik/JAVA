56f1ed735696cd79adf6e9c6f97a0e55fcab2534
benniimaru <shaikabdulmalik185@gmail.com>
2024-10-09 03:23:47 +0530

    strings

---
+++ mix.java
@@ -0,0 +1,14 @@
+public class mix {
+    public static void main (String[] args)
+    {
+        String x = "Water";
+        String y = "Tang";
+
+        // x=y; both gives Tang
+        // y=x; both gives Water
+
+        System.out.println("x : "+x);
+        System.out.println("y : "+y);
+
+    }
+}
