77d91ee3f4f5720258bbad2b5b53777ab0e02e09
benniimaru <shaikabdulmalik185@gmail.com>
2024-10-16 21:46:46 +0530

    Operators

---
+++ Expressions.java
@@ -0,0 +1,14 @@
+public class Expressions {
+    public static void main(String[] args) {
+        
+        double friends = 10;
+        //friends = friends + 1;
+        //friends = friends - 1;
+        //friends = friends * 2;
+        //friends = friends / 5;
+        //friends = friends % 2;
+        //friends = friends / 3; //This is used to change the datatype as int datatype does not show decimals
+        System.out.println(friends);
+
+    }
+}
