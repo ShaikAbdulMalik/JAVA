84854b0e247efa89c5bee6742be434bb7b769d7a
benniimaru <shaikabdulmalik185@gmail.com>
2024-10-23 07:02:00 +0530

    gui update

--- GUI.java
+++ GUI.java
@@ -7,7 +7,9 @@ public class GUI {
         JOptionPane.showMessageDialog(null, "Hello!! "+name);
         
         int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));//parseint allows you to change a datatype from string to int
-        JOptionPane.showMessageDialog(null, "You are "+age+"Years old");
-
+        JOptionPane.showMessageDialog(null, "You are "+age+" Years old");
+        
+        double Height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height "));//parseint allows you to change a datatype from string to double
+        JOptionPane.showMessageDialog(null, "You are "+Height+" feet Tall");
     }
 }
